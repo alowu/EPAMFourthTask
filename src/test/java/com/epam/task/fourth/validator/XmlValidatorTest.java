9a5bb10d7b0c919e30686acb8d4b09f725746d28
alo_wu <ilya.bela@yandex.ru>
2021-02-12 23:11:55 +0300

    add: validator test

---
+++ src/test/java/com/epam/task/fourth/validator/XmlValidatorTest.java
@@ -0,0 +1,26 @@
+package com.epam.task.fourth.validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class XmlValidatorTest {
+    private XmlValidator validator = new XmlValidator();
+
+    private final String XSD_FILE = "src/main/resources/gems.xsd";
+    private final String VALID_XML = "src/main/resources/gems.xml";
+    private final String INVALID_XML = "/main/resources/g.xml";
+
+    @Test
+    public void testXmlValidatorShouldPassWhenCorrectFile() {
+        boolean result = validator.isValid(VALID_XML, XSD_FILE);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testXmlValidatorShouldPassWhenInvalidFile() {
+        boolean result = validator.isValid(INVALID_XML, XSD_FILE);
+
+        Assert.assertFalse(result);
+    }
+}
