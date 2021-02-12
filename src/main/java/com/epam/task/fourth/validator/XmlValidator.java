c0e8e2603f01d50dbae3fb585439dd83e9dfa21c
alo_wu <ilya.bela@yandex.ru>
2021-02-12 23:12:22 +0300

    add: validator class

---
+++ src/main/java/com/epam/task/fourth/validator/XmlValidator.java
@@ -0,0 +1,43 @@
+package com.epam.task.fourth.validator;
+
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class XmlValidator {
+    private final Logger LOGGER = Logger.getLogger(XmlValidator.class);
+
+    public boolean isValid(String fileName, String schemaName){
+        String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        SchemaFactory factory = SchemaFactory.newInstance(language);
+        File schemaLocation = new File(schemaName);
+        Schema schema = null;
+
+        boolean valid = false;
+
+        try {
+            schema = factory.newSchema(schemaLocation);
+            Validator validator = schema.newValidator();
+            Source source = new StreamSource(fileName);
+            validator.validate(source);
+
+            LOGGER.info(fileName + " is valid");
+            valid = true;
+        } catch (SAXException e) {
+            LOGGER.error("Validation " + fileName + " isn't valid cause " +
+                    e.getMessage());
+        } catch (IOException e) {
+            LOGGER.error("Validation " + fileName + " isn't valid cause " +
+                    e.getMessage());
+        }
+        return valid;
+    }
+}
