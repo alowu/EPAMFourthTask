d2a7c1e950c703180778caa83494d83b7d4ebf15
alo_wu <ilya.bela@yandex.ru>
2021-02-15 01:53:24 +0300

    add: abstract test for parsers

---
+++ src/test/java/com/epam/task/fourth/parser/AbstractParserTest.java
@@ -0,0 +1,39 @@
+package com.epam.task.fourth.parser;
+
+import com.epam.task.fourth.entity.Gems;
+import com.epam.task.fourth.entity.Precious;
+import com.epam.task.fourth.entity.Semiprecious;
+import com.epam.task.fourth.validator.XmlException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class AbstractParserTest {
+    private AbstractParser parser;
+    private final String VALID_FILE = "inputData/gems.xml";
+    private final String INVALID_FILE = "inputData/g.xml";
+
+    private final List<Gems> EXPECTED = Arrays.asList(
+            new Precious("p01", "diamond", "white", 150, 0, 80, 10),
+            new Precious("p02", "ruby", "red", 100, 5, 100, 15),
+            new Semiprecious("s01", "apatite", "blue", 70, 0, 70),
+            new Semiprecious("s02", "prehnite", "green", 200, 10, 50));
+
+    public void setParser(AbstractParser parser) {
+        this.parser = parser;
+    }
+
+    @Test
+    public void testParseShouldParseValidXmlFileInList() throws XmlException {
+        List<Gems> result = parser.parseListGems(VALID_FILE);
+
+        Assert.assertEquals(EXPECTED, result);
+    }
+
+    @Test(expected = XmlException.class)
+    public void testParseShouldThrowExceptionWhenXmlInvalid() throws XmlException {
+        parser.parseListGems(INVALID_FILE);
+    }
+}
