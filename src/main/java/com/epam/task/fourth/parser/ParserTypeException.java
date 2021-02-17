9daa242d65d8084b692af301a398d09cab07b3af
alo_wu <ilya.bela@yandex.ru>
2021-02-17 14:59:30 +0300

    add: parserType exception

---
+++ src/main/java/com/epam/task/fourth/parser/ParserTypeException.java
@@ -0,0 +1,10 @@
+package com.epam.task.fourth.parser;
+
+public class ParserTypeException extends Exception{
+    public ParserTypeException() {
+    }
+
+    public ParserTypeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
