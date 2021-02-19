011ece329a4bcea19d9845d409030587113c6885
alo_wu <ilya.bela@yandex.ru>
2021-02-19 15:02:46 +0300

    remove stax

--- src/test/java/com/epam/task/fourth/parser/StaxParserTest.java
+++
@@ -1,9 +0,0 @@
-package com.epam.task.fourth.parser;
-
-public class StaxParserTest extends AbstractParserTest {
-
-    @Override
-    protected XmlParser getParser() {
-        return new StaxParser();
-    }
-}
